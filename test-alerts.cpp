9fed1a9809d5f55fc750bda291de05cfbd220f61
PRAVEEN7493 <37633227+PRAVEEN7493@users.noreply.github.com>
2023-07-03 19:39:41 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -2,7 +2,11 @@
 
 #include "test/catch.hpp"
 #include "typewise-alert.h"
-
+//double value, double lowerLimit, double upperLimit) {
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+
+  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+
+  REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);
 }
